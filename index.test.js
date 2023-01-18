e3d640dfaf5556b5e7c9bd49ed81521974e80ec6
codingmd <21052985+codingmd@users.noreply.github.com>
2023-01-18 19:20:57 +0000

    add get indgredient test

--- index.test.js
+++ index.test.js
@@ -9,9 +9,15 @@ describe("index", () => {
        expect(maxCakes).toBe(1);
     });
 
-    it("get recipe return recipe1 for recipe1", ()=> {
+    it("get recipe returns recipe1 for recipe1", ()=> {
         recipe1 = {flour: 1};
  
         expect(bakery.recipe(recipe1)).toBe(recipe1);
+     });
+
+     it("get ingredients returns ingredients1 for ingredients1", ()=> {
+        ingredients1 = {flour: 1};
+ 
+        expect(bakery.ingredients(ingredients1)).toBe(ingredients1);
      });
 });
