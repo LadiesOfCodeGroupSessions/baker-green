d9943812d1ea3ab8003e04a104f267a343cf364b
codingmd <21052985+codingmd@users.noreply.github.com>
2023-01-18 19:14:33 +0000

    added new get recipe test

--- index.test.js
+++ index.test.js
@@ -1,11 +1,17 @@
 const bakery = require("./index.js");
 
 describe("index", () => {
-    it("", ()=> {
+    it("maxCakes for recipe1 is 1", ()=> {
        recipe1 = {flour: 1};
        ingredients = {flour: 1};
 
        let maxCakes = bakery.cakes(recipe1, ingredients);
        expect(maxCakes).toBe(1);
     });
+
+    it("get recipe return recipe1 for recipe1", ()=> {
+        recipe1 = {flour: 1};
+ 
+        expect(bakery.recipe(recipe1)).toBe(recipe1);
+     });
 });
